08a34134a22e7f48e8a88d7bcf19edf5ec78c0ca
Syafiq Rizky Fauzi <bussinesyafiq@gmail.com>
2023-10-05 13:22:49 +0700

    add : mahasiswa & feedback migrations

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\FeedbackController;
+use App\Http\Controllers\MahasiswaController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/feedback', [FeedbackController::class, 'index']);
+Route::post('/feedback', [FeedbackController::class, 'store']);
+Route::put('/feedback/{id}', [FeedbackController::class, 'update']);
+
+Route::post('/mahasiswa/register', [MahasiswaController::class, 'register']);
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::get('/mahasiswa/{id}', [MahasiswaController::class, 'show']);
+Route::put('/mahasiswa/{id}', [MahasiswaController::class, 'update']);
+Route::delete('/mahasiswa/{id}', [MahasiswaController::class, 'destroy']);
